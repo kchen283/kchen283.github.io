c5b30f0e760ee3d3c4041ed1d128d867c4f62124
kchen283 <kchen1270@gmail.com>
2023-01-23 11:29:27 -0800

    add intro section

---
+++ script.js
@@ -0,0 +1,17 @@
+var i = 0;
+var txt = 'Hi! Welcome to my website, my name is Keri. Nice to meet you :)';
+var speed = 50;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("section1Text").innerHTML += txt.charAt(i);
+    if (i == 3) {
+        document.getElementById("section1Text").innerHTML += "<br />";
+    }   
+    if (i == 42) {
+        document.getElementById("section1Text").innerHTML += "<br />";
+    }   
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
