ae9f2a6186f911b418f0f2300911e21075db415d
kchen283 <kchen1270@gmail.com>
2024-08-28 23:42:24 -0400

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,6 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import { HashRouter as Router, Route, Routes, Navigate, useLocation } from 'react-router-dom';
 
 const App = () => {
-  const code = new URLSearchParams(location.search).get('code');
 
   const scrollToTop = () => {
     window.scrollTo({
@@ -21,27 +20,11 @@ const App = () => {
       behavior: 'smooth', // This enables smooth scrolling
     });
   };
-
-  // Function to remove code from URL
- // const cleanUrl = () => {
-  //  const url = new URL(window.location.href);
-   // url.searchParams.delete('code');
-    //window.history.replaceState({}, '', url);
-  //};
-
-  // Effect to handle URL cleanup if code is present
- // useEffect(() => {
-  //  if (code) {
-   //   cleanUrl();
-   // }
-  // }, [code]);
-
   
   return (
     <Router>
       <Routes>
-        {/* Redirect to Spotify if code exists, otherwise show home page */}
-        <Route path="/"  element={code ? <Navigate to="/Spotify API" /> :(
+        <Route path="/"  element={(
           <div>
             <section id="Home">
               <Navbar />
--- src/spotify/Login.jsx
+++ src/spotify/Login.jsx
@@ -2,12 +2,12 @@ import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './login.scss';
 
-const AUTH_URL = "https:/accounts.spotify.com/authorize?client_id=7311656353e9483bbc471f86c5c721f6&response_type=code&redirect_uri=https://spotifycolorring-kchen283s-projects.vercel.app/api/callback&scope=user-top-read%20user-read-recently-played";
+const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=7311656353e9483bbc471f86c5c721f6&response_type=code&redirect_uri=https://spotifycolorring-kchen283s-projects.vercel.app/api/callback&scope=user-top-read%20user-read-recently-played";
 
 const Login = () => {
   return (
     <div className="login-container">
-      <a className="login-button" href={AUTH_URL}>Login with Spotify</a>
+      <a className="login-button" href={AUTH_URL} target="_blank" rel="noopener noreferrer">Login with Spotify</a>
     </div>
   );
 };
